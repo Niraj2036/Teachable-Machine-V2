eebba692a48e28c50936567753a9c725e08fe34b
Niraj2036 <160124153+Niraj2036@users.noreply.github.com>
2024-05-14 01:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,36 @@ import streamlit as st
 
 def main():
     st.title("Teachable Machine V2")
-    st.write("Train the model of")
+    st.write("Train the model of:")
 
     # Classification and Regression buttons with embedded URLs
     col1, col2 = st.columns(2)
     with col1:
         
-        st.write('<a href="https://classification-teachable-machine.streamlit.app" style="text-decoration: none; color: inherit;"><button style="padding: 10px 20px; background-color: #008CBA; color: white; border: none; border-radius: 5px; cursor: pointer;">Classification</button></a>', unsafe_allow_html=True)
+        st.write(
+            '''
+            <style>
+                .custom-button {
+                padding: 10px 20px;
+                background-color: white;
+                color: red;
+                border: 2px solid red;
+                border-radius: 5px;
+                cursor: pointer;
+                }
+                .custom-button:hover {
+                background-color: red;
+                color: white;
+                border-color: white;
+                }
+            </style>
+            <a href="https://classification-teachable-machine.streamlit.app" style="text-decoration: none; color: red;">
+            <button class="custom-button">Classification</button>
+            </a>
+            ''', 
+            unsafe_allow_html=True
+)
+
     with col2:
         
         st.write('<a href="https://regression-teachable-machine.streamlit.app" style="text-decoration: none; color: inherit;"><button style="padding: 10px 20px; background-color: #008CBA; color: white; border: none; border-radius: 5px; cursor: pointer;">Regression</button></a>', unsafe_allow_html=True)
