fcae16f7e4b328f73da1433f37532fd983bbe84a
Niraj2036 <160124153+Niraj2036@users.noreply.github.com>
2024-05-14 00:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,8 @@ def main():
     st.title("Teachable Machine V2")
     st.write("Train the model of")
 
-    # Button container
-    col1, col2 = st.columns(2)
-
-    # Classification button
-    with col1:
-        st.markdown("[Classification](https://classification-teachable-machine.streamlit.app)")
-
-    # Regression button
-    with col2:
-        st.markdown("[Regression](https://regression-teachable-machine.streamlit.app)")
+    # Classification and Regression buttons
+    st.markdown("[Classification](https://classification-teachable-machine.streamlit.app) [Regression](https://regression-teachable-machine.streamlit.app)")
 
 if __name__ == "__main__":
     main()
